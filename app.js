31ed6b0d74a1f0186d29d2f473db7913ccd7928e
zaindhedhi <zaindhedhi@oulook.com>
2024-07-04 14:24:28 +0500

    COMPLETED

---
+++ app.js
@@ -0,0 +1,86 @@
+// percentage calculator
+
+var totalMarks = 400;
+
+var engMarks = +prompt('Enter Your English Marks');
+var chemMarks = +prompt('Enter Your Chemistry Marks');
+var phyMarks = +prompt('Enter Your Physics Marks');
+var urduMarks = +prompt('Enter Your Urdu Marks');
+
+var obtainedMarks = engMarks + phyMarks + chemMarks + urduMarks
+
+var percentage = obtainedMarks / totalMarks * 100;
+
+console.log('you got ' + percentage + '%')
+
+
+// Assignment no 1 "Topic Alerts"
+
+// Q no 1
+// var greetUser = 'Hello JavaScript!';
+// alert(greetUser);
+
+// Qno 2
+// alert("Error! Please Enter a Valid Password.");
+
+// Qno 3
+
+
+
+
+
+
+// Assignment no 2 "Topic Variables"
+
+// Qno 1
+// var username = "test";
+// console.log(username);
+
+// Q no2
+// var myName = 'Muhammad Zain'
+// console.log(myName);
+
+// Qno 3
+// var title = "Hello World!"
+// alert(title);
+
+// Qno 4
+// var name = "John Doe"
+// alert(name);
+
+// var age = '15 years old'
+// alert(age);
+
+// var education = 'certified mobile app developer'
+// alert(education);
+
+// Qno 5
+// var pizza = 'PIZZA'
+// var pizz = 'PIZZ'
+// var piz = 'PIZ'
+// var pi = 'PI'
+// var p = 'P'
+
+// alert(pizz);
+
+// // Qno 6
+// var email = 'zaindhedhi@outlook.com'
+// alert("My Email Address is " + email);
+
+// Qno 7
+// var book = 'A smarter way to learn Javascript'
+// alert("Iam trying to learn from the Book " + book);
+
+// var num1 = 3
+// var num2 = 5
+
+// var multi = num1 * num2;
+// console.log("sum of 3 and 5 is " + multi)
+
+// var perPrice = 600;
+// var quant = 5 ;
+// var total = perPrice*quant;
+
+// console.log("Total cost of 5 tickets to a movie is " + total);
+
+
